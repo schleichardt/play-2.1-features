5302d8c26ea02db122de046b0a36b61ee584df46
Michael Schleichardt <michael@schleichardt.net>
2013-02-18 23:33:36 +0100

    init Play 2.1 Scala application

---
+++ project/Build.scala
@@ -0,0 +1,22 @@
+import sbt._
+import Keys._
+import play.Project._
+
+object ApplicationBuild extends Build {
+
+  val appName         = "play-2-1-features"
+  val appVersion      = "1.0-SNAPSHOT"
+
+  val appDependencies = Seq(
+    // Add your project dependencies here,
+    jdbc,
+    anorm,
+    "junit" % "junit-dep" % "4.11" % "test"
+  )
+
+
+  val main = play.Project(appName, appVersion, appDependencies).settings(
+    // Add your own project settings here      
+  )
+
+}
